0939f66859090549d865486805255051e146b848
Fusino_Tatsuya <silverlight0522@gmail.com>
2025-05-11 17:22:50 +0900

    스크립트로 파일 생성

---
+++ Assets/1_Scenes/Editor/NewBehaviourScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEditor;
+using UnityEngine;
+using System.IO;
+
+public class FolderStructureGenerator
+{
+    [MenuItem("Tools/Create Default Folder Structure")]
+    public static void CreateFolderStructure()
+    {
+        string[] folders = new string[]
+        {
+            "Art/A_KYR",
+            "Art/A_KDY",
+            "Audio/BGM",
+            "Audio/SFX",
+            "Materials",
+            "Prefabs/Characters",
+            "Prefabs/Enemies",
+            "Prefabs/UI",
+            "0_Scenes/P_Euni",
+            "0_Scenes/P_CTH",
+            "0_Scenes/P_PJH",
+            "0_Scenes/P_JYS",
+            "1_Scripts/P_Euni",
+            "1_Scripts/P_CTH",
+            "1_Scripts/P_PJH",
+            "1_Scripts/P_JYS",
+            "Animations",
+            "Fonts",
+            "Resources"
+        };
+
+        foreach (string folder in folders)
+        {
+            string folderPath = Path.Combine("Assets", folder);
+            if (!AssetDatabase.IsValidFolder(folderPath))
+            {
+                Directory.CreateDirectory(folderPath);
+                Debug.Log("Created folder: " + folderPath);
+            }
+        }
+
+        AssetDatabase.Refresh();
+    }
+}
